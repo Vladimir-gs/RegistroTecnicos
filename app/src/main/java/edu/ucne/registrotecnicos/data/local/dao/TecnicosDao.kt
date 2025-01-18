8b10ca277bb0109649c02d4481e2d0c24a9ad90c
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-18 13:16:20 -0400

    TecnicoDao ha sido movido a la carpeta dao

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/dao/TecnicosDao.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/dao/TecnicosDao.kt
@@ -3,9 +3,9 @@ package edu.ucne.registrotecnicos.data.local.dao
 import androidx.room.Delete
 import androidx.room.Query
 import androidx.room.Upsert
-import edu.ucne.registrotecnicos.MainActivity.TecnicosEntity
 import kotlinx.coroutines.flow.Flow
 import androidx.room.Dao;
+import edu.ucne.registrotecnicos.data.local.entity.TecnicosEntity
 
 @Dao
 interface TecnicosDao {
