dc74506450691d60f820b1cc739a451b75ba7387
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-19 13:36:01 -0400

    Modificamos NavHost para la navegacion entre las diferentes pantallas

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TecnicoNavHost.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/TecnicoNavHost.kt
@@ -7,6 +7,8 @@ import androidx.navigation.compose.rememberNavController
 import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
 import edu.ucne.registrotecnicos.presentation.tecnico.TecnicoListScreen
 import edu.ucne.registrotecnicos.presentation.tecnico.TecnicoScreen
+import edu.ucne.registrotecnicos.presentation.ticket.TicketListScreen
+import edu.ucne.registrotecnicos.presentation.ticket.TicketScreen
 
 @Composable
 fun TecnicoNavHost(tecnicoDb: TecnicoDb) {
@@ -24,6 +26,18 @@ fun TecnicoNavHost(tecnicoDb: TecnicoDb) {
         }
         composable("agregarTecnico") {
             TecnicoScreen(
+                navController = navController,
+                tecnicoDb = tecnicoDb
+            )
+        }
+        composable("agregarTicket") {
+            TicketScreen(
+                navController = navController,
+                tecnicoDb = tecnicoDb
+            )
+        }
+        composable("listaTickets") {
+            TicketListScreen(
                 navController = navController,
                 tecnicoDb = tecnicoDb
             )
