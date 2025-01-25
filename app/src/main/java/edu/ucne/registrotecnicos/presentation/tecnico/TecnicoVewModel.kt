01f0e57cfac1838cec56045d1de631d0fb29e259
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-25 00:49:32 -0400

    Modificaciones a mi TecnicoVewModel.kt

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/tecnico/TecnicoVewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/tecnico/TecnicoVewModel.kt
@@ -2,20 +2,106 @@ package edu.ucne.registrotecnicos.presentation.tecnico
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import edu.ucne.registrotecnicos.data.local.database.TecnicoDb
+import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.registrotecnicos.data.local.entity.TecnicosEntity
+import edu.ucne.registrotecnicos.data.repository.TecnicoRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TecnicoViewModel(private val tecnicoDb: TecnicoDb) : ViewModel() {
-    fun deleteTecnico(tecnico: TecnicosEntity) {
-        viewModelScope.launch {
-            tecnicoDb.tecnicoDao().delete(tecnico)
+@HiltViewModel
+class TecnicoViewModel @Inject constructor(
+    private val repository: TecnicoRepository,
+) : ViewModel() {
+    private val _uiState = MutableStateFlow(Uistate())
+    val uiState = _uiState.asStateFlow()
+
+    init {
+        getTecnicos()
+    }
+
+    fun save(){
+        if (uiState.value.nombre.isBlank() || uiState.value.sueldo <= 0) {
+           _uiState.update {
+               it.copy(errorMessage = "Verifique los datos")
+           }
+        }else{
+            viewModelScope.launch {
+                repository.save(uiState.value.toEntity())
+            }
+            nuevo()
         }
     }
 
-    fun saveTecnico(tecnico: TecnicosEntity) {
+    fun selectedTecnico(tecnicoId: Int) {
         viewModelScope.launch {
-            tecnicoDb.tecnicoDao().save(tecnico)
+            if (tecnicoId > 0) {
+                val tecnico = repository.getTecnico(tecnicoId)
+                _uiState.update {
+                    it.copy(
+                        tecnicosId = tecnico?.tecnicosId,
+                        nombre = tecnico?.nombre ?: "",
+                        sueldo = tecnico?.sueldo ?: 0.0
+                    )
+                }
+
+            }
         }
+    }
+
+    fun delete() {
+        viewModelScope.launch {
+            repository.delete(_uiState.value.toEntity())
+        }
+    }
+
+    private fun getTecnicos() {
+        viewModelScope.launch {
+            repository.getAll().collect { tecnicos ->
+                _uiState.update {
+                    it.copy(tecnicos = tecnicos)
+                }
+            }
+        }
+    }
+
+    fun onNameChange(name: String) {
+        _uiState.update {
+            it.copy(
+                nombre = name,
+                errorMessage = if (name.isBlank()) "No se Puede Guardar con nombre Vacío"
+                else null
+            )
+        }
+    }
+
+    fun onSalaryChange(salary: Double) {
+        _uiState.update {
+            it.copy(
+                sueldo = salary,
+                errorMessage = if (salary < 0.0) "No se Puede Guardar con sueldo Negativo"
+                else null
+            )
+        }
+    }
+
+    fun nuevo() {
+        _uiState.value = Uistate()
     }
 }
+
+fun Uistate.toEntity() = TecnicosEntity(
+    tecnicosId = this.tecnicosId,
+    nombre = this.nombre,
+    sueldo = this.sueldo
+)
+
+data class Uistate(
+    val tecnicosId: Int? = null,
+    val nombre: String = "",
+    val sueldo: Double = 0.0,
+    val errorMessage: String? = null,
+    val tecnicos: List<TecnicosEntity> = emptyList(),
+)
