88bb345a2865cca2a7f26f3fe8559066f48964e6
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-25 00:48:40 -0400

    Agregué todos los Screens faltantes en mi Screen.kt

--- app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/presentation/navigation/Screen.kt
@@ -3,9 +3,33 @@ package edu.ucne.registrotecnicos.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
+
     @Serializable
-    object TecnicoList : Screen()
+    data object HomeScreen: Screen()
+
     @Serializable
-    data class Tecnico(val tecnicoId: Int) : Screen()
+    data object TecnicoList : Screen()
+
+    @Serializable
+    data class Tecnico(val TecnicoId: Int): Screen()
+
+    @Serializable
+    data class TecnicoDelete(val TecnicoId: Int) : Screen()
+
+    @Serializable
+    data class TecnicoEdit(val TecnicoId: Int) : Screen()
+
+    @Serializable
+    data object TicketList: Screen()
+
+    @Serializable
+    data class Ticket(val TicketId: Int): Screen()
+
+    @Serializable
+    data class TicketDelete(val TicketId: Int) : Screen()
+
+    @Serializable
+    data class TicketEdit(val TicketId: Int) : Screen()
+
 
 }
