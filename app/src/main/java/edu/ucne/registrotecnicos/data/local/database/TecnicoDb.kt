b84bf25e2e36d61861eec6937b0bf4e52c95dc29
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-27 11:40:44 -0400

    Agregué el Converters.kt para el manejo de fechas en el proframa

--- app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnicos/data/local/database/TecnicoDb.kt
@@ -2,6 +2,7 @@ package edu.ucne.registrotecnicos.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import edu.ucne.registrotecnicos.data.local.dao.MensajeDao
 import edu.ucne.registrotecnicos.data.local.dao.TecnicosDao
 import edu.ucne.registrotecnicos.data.local.dao.TicketDao
@@ -11,9 +12,10 @@ import edu.ucne.registrotecnicos.data.local.entity.TicketsEntity
 
 @Database(
     entities = [TecnicosEntity::class, TicketsEntity::class, MensajeEntity::class],
-    version = 4,
+    version = 5,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicosDao
     abstract fun ticketDao(): TicketDao
